ded8f994eca2f4acc9a4ad499b518b7f979e36c4
Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>
2025-01-27 21:19:10 +0000

    Add more test cases

--- tests/Translation/TranslationPotentiallyTranslatedStringTest.php
+++ tests/Translation/TranslationPotentiallyTranslatedStringTest.php
@@ -19,4 +19,35 @@ class TranslationPotentiallyTranslatedStringTest extends TestCase
         $translation->translate();
         $this->assertSame('world', (string) $translation);
     }
+
+    public function testPotentiallyTranslatedStringWithReplacement()
+    {
+        $translator = new Translator(tap(new ArrayLoader)->addMessages('en', 'custom', [
+            'message' => 'hello :recipient',
+        ]), 'en');
+        $translation = new PotentiallyTranslatedString('custom.message', $translator);
+        $translation->translate([ 'recipient' => 'world' ]);
+        $this->assertSame('hello world', (string) $translation);
+    }
+
+    public function testPotentiallyTranslatedStringWithoutFallback()
+    {
+        $translator = new Translator(tap(new ArrayLoader)->addMessages('en', 'custom', [
+            'hello' => 'world',
+        ]), 'en');
+        $translation = new PotentiallyTranslatedString('custom.hello', $translator);
+        $translation->translate(locale: 'de');
+        $this->assertSame('custom.hello', (string) $translation);
+    }
+
+    public function testPotentiallyTranslatedStringWithFallback()
+    {
+        $translator = new Translator(tap(new ArrayLoader)->addMessages('en', 'custom', [
+            'hello' => 'world',
+        ]), 'en');
+        $translator->setFallback('en');
+        $translation = new PotentiallyTranslatedString('custom.hello', $translator);
+        $translation->translate(locale: 'de');
+        $this->assertSame('world', (string) $translation);
+    }
 }
