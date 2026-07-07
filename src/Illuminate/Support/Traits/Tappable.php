577c0f3107c3c0897576c9f90d43fecdd65ef836
Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>
2026-07-08 00:45:46 +0200

    Mark `$callback` as immediately invoked

--- src/Illuminate/Support/Traits/Tappable.php
+++ src/Illuminate/Support/Traits/Tappable.php
@@ -8,6 +8,7 @@ trait Tappable
      * Call the given Closure with this instance then return the instance.
      *
      * @param  (callable($this): mixed)|null  $callback
+     * @param-immediately-invoked-callable  $callback
      * @return ($callback is null ? \Illuminate\Support\HigherOrderTapProxy : $this)
      */
     public function tap($callback = null)
